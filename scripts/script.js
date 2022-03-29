5bcccc079ee74dbecd7111fa7cf0a71cc1ff70bf
Влада <vladaoboenkova@gmail.com>
2022-03-29 18:32:22 +0300

    Added function to convert text to sentence case.

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,33 @@ document.getElementById("proper-case").addEventListener("click", function(){
     let text = document.getElementById("textarea").value;
     document.getElementById("textarea").value = toProperCase(text);
 })
+
+function toSentenceCase(text){
+    let sentences = text.split(/[.?!]/);
+    let sentenceCaseText = "";
+    let characters = [...text];
+    let textMarks = [];
+    for (let i = 0; i < characters.length; i++){
+        if (characters[i] === "?" || characters[i] === "." || characters[i] === "!"){
+            textMarks.push(characters[i]);
+        }
+    }
+    sentences.forEach((sentence, index) => {
+        if (sentence.charAt(0) !== " ") {
+            sentences[index] = sentence.charAt(0).toUpperCase() + sentence.slice(1).toLowerCase();
+        } else {
+            let i = 1;
+            while (sentence.charAt(i) === " "){
+                i++;
+            }
+            sentences[index] = sentence.charAt(i).toUpperCase() + sentence.slice(i + 1).toLowerCase();
+        }
+        if (index !== sentences.length - 1) {
+            sentenceCaseText += sentences[index] + textMarks[index] + " ";
+        } else {
+            sentenceCaseText += sentences[index];
+        }
+
+    })
+    return sentenceCaseText.trim();
+}
