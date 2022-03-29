c6c2fd7864bd5e47a7b7b0d3a98441335f5c4003
Влада <vladaoboenkova@gmail.com>
2022-03-29 18:11:53 +0300

    Added function to convert text to proper case.

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,14 @@ document.getElementById("lower-case").addEventListener("click", function(){
     let text = document.getElementById("textarea").value;
     document.getElementById("textarea").value = text.toLowerCase();
 })
+
+function toProperCase(text){
+    let words = text.split(" ");
+    let properCaseText = "";
+    words.forEach((word, index) => {
+        words[index] = word.charAt(0).toUpperCase() + word.slice(1);
+        properCaseText += words[index] + " ";
+    })
+    return properCaseText.trim();
+}
+
