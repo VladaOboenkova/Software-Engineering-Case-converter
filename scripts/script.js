d74210728d897dd8c28456caa7ad9108c670a95d
Влада <vladaoboenkova@gmail.com>
2022-03-29 18:43:04 +0300

    Added function to download text.

--- scripts/script.js
+++ scripts/script.js
@@ -59,3 +59,16 @@ document.getElementById("sentence-case").addEventListener("click", function(){
     let text = document.getElementById("textarea").value;
     document.getElementById("textarea").value = toSentenceCase(text);
 })
+
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
